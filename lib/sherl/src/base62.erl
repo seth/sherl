7bba270666fc0f8daf80b301f0c93dd8f39420bd
Seth Falcon <seth@userprimary.net>
2009-06-07 08:53:38 -0700

    Add base62 module and test suite

---
+++ lib/sherl/src/base62.erl
@@ -0,0 +1,39 @@
+-module(base62).
+
+-export([encode/1, decode/1, digit_to_char/1, char_to_digit/1]).
+
+%% @spec encode(integer()) -> string()
+%% @doc encode an integer into base 62 using 0-9, A-Z, and a-z
+encode(I) ->
+    encode(I, []).
+
+%% @spec decode(string()) -> integer()
+%% @doc decode a string encoded in base 62 to an integer
+decode(S) ->
+    decode(lists:reverse(S), 1, 0).
+
+digit_to_char(D) when D < 10 ->
+    $0 + D;
+digit_to_char(D) when D < 36 ->
+    $0 + 7 + D;
+digit_to_char(D) when D < 62 ->
+    $0 + 13 + D.
+
+char_to_digit(C) when C < (10 + $0) ->
+    C - $0;
+char_to_digit(C) when C < (43 + $0) ->
+    C - 7 - $0;
+char_to_digit(C) when C < (75 + $0) ->
+    C - 13 - $0.
+
+encode(I, Acc) when I < 62 ->
+    [digit_to_char(I) | Acc];
+encode(I, Acc) ->
+    I1 = I div 62,
+    encode(I1, [digit_to_char(I rem 62)| Acc]).
+
+decode([C | T], N, Acc) ->
+    decode(T, N * 62, char_to_digit(C) * N + Acc);
+decode([], _N, Acc) ->
+    Acc.
+
---
+++ lib/sherl/test/base62_SUITE.erl
@@ -0,0 +1,68 @@
+-module(base62_SUITE).
+
+-compile(export_all).
+
+-include("../include/url.hrl").
+-include_lib("ct.hrl").
+
+%%--------------------------------------------------------------------
+%% Function: suite() -> DefaultData
+%% DefaultData: [tuple()]
+%% Description: Require variables and set default values for the suite
+%%--------------------------------------------------------------------
+suite() -> [{timetrap,{seconds,10}}].
+
+%%--------------------------------------------------------------------
+%% Function: init_per_suite(Config) -> Config
+%% Config: [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Description: Initiation before the whole suite
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% Function: end_per_suite(Config) -> _
+%% Config: [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Description: Cleanup after the whole suite
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Function: all() -> TestCases
+%% TestCases: [Case]
+%% Case: atom()
+%%   Name of a test case.
+%% Description: Returns a list of all test cases in this test suite
+%%--------------------------------------------------------------------
+all() ->
+    [digit_to_char_test].
+
+%%-------------------------------------------------------------------------
+%% Test cases starts here.
+%%-------------------------------------------------------------------------
+
+digit_to_char_test() ->
+    [{userdata,[{doc,"digit to 0-9A-Za-z"}]}].
+
+digit_to_char_test(_Config) ->
+    [ Char = base62:digit_to_char(Digit) ||
+        {Digit, Char} <- lists:zip(all_digits(), all_chars()) ],
+    ok.
+
+%% internal helpers
+
+all_chars() ->
+    [$0, $1, $2, $3, $4, $5, $6, $7, $8, $9, $A, $B, $C, $D, $E, $F,
+     $G, $H, $I, $J, $K, $L, $M, $N, $O, $P, $Q, $R, $S, $T, $U, $V,
+     $W, $X, $Y, $Z, $a, $b, $c, $d, $e, $f, $g, $h, $i, $j, $k, $l,
+     $m, $n, $o, $p, $q, $r, $s, $t, $u, $v, $w, $x, $y, $z].
+
+all_digits() ->
+    lists:seq(0, 61).
+
