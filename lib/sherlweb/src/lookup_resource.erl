7b724c6deac3db7791504af7b20d3b1a2372a90b
Seth Falcon <seth@userprimary.net>
2009-06-28 19:05:00 -0700

    Return 301 or 404

--- lib/sherlweb/src/lookup_resource.erl
+++ lib/sherlweb/src/lookup_resource.erl
@@ -1,18 +1,37 @@
-%% @author author <author@example.com>
-%% @copyright YYYY author.
-%% @doc Example webmachine_resource.
+%% @author Seth Falcon <seth@userprimary.net>
+%% @copyright 2009 Seth Falcon.
+%% @doc The sherl lookup resource expects sherl codes and returns
+%% either a 301 redirect or a 404, if the code was not found.
 
 -module(lookup_resource).
--export([init/1, to_html/2]).
+-export([init/1, moved_permanently/2, previously_existed/2,
+         resource_exists/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
-init([]) -> {ok, undefined}.
+-record(sherl, {long_url = undefined}).
 
-to_html(ReqData, State) ->
-    {ok, LongUrl} = sherl:decode(wrq:disp_path(ReqData)),
-    {["<html><body><a href=\"", LongUrl, "\">", LongUrl,
-      "</a></body></html>"],
-     ReqData, State}.
+init([]) -> {ok, #sherl{}}.
 
+previously_existed(ReqData, State) ->
+    case State#sherl.long_url of
+        undefined ->
+            {false, ReqData, State};
+        _ ->
+            {true, ReqData, State}
+    end.
 
+moved_permanently(ReqData, State) ->
+    case State#sherl.long_url of
+        undefined ->
+            {false, ReqData, State};
+        LongUrl ->
+            {{true, LongUrl}, ReqData, State}
+    end.
+
+resource_exists(ReqData, State) ->
+    {Status, LongUrl} = sherl:decode(wrq:disp_path(ReqData)),
+    case Status of
+        ok -> {false, ReqData, State#sherl{long_url = LongUrl}};
+        _ -> {false, ReqData, State}
+    end.
